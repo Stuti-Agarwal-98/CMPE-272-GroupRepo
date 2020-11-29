caec55fbcec84b59b5b6affca8f282be72ecb787
Haley <haleyhfeng@gmail.com>
2020-11-28 17:05:31 -0800

    Update with serverless

---
+++ Final_Project/daemon-db/config/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_OtQIK3Cpp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '45pbll8lec37cfhckq4r39d5dr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://d7rlzfc25d.execute-api.us-east-2.amazonaws.com/dev/employees' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
